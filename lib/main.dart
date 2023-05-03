eb7b580838d28f8e48766066117ef7849a18e027
Gourav Kumar <91111185+gourav2982@users.noreply.github.com>
2023-05-03 17:53:29 +0530

    practice 4 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/LoginPage.dart';
 import 'package:flutter_catalog/pages/HomePage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.light(),
-      initialRoute: "/Home",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      initialRoute: "/Login",
       routes: {
         "/": (context) => const HomePage(),
         "/Home": (context) => const HomePage(),
--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -5,9 +5,45 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Login Page"),
+    return Material(
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/Login_image.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          const Text(
+            'welcome',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Username',
+                    labelText: 'Username',
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      hintText: 'Enter Password', labelText: 'Password'),
+                )
+              ],
+            ),
+          ),
+          ElevatedButton(
+            child: Text('Login'),
+            onPressed: () {
+              print("hi");
+            },
+          )
+        ],
       ),
     );
   }
